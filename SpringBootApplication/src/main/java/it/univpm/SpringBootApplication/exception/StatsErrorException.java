89735e6d978c508ff52abf6a282962128b47434d
Erxhes Dedja <91878765+Erxhes@users.noreply.github.com>
2021-11-26 16:16:06 +0100

    Update StatsErrorException.java

--- SpringBootApplication/src/main/java/it/univpm/SpringBootApplication/exception/StatsErrorException.java
+++ SpringBootApplication/src/main/java/it/univpm/SpringBootApplication/exception/StatsErrorException.java
@@ -1,11 +1,21 @@
 package it.univpm.SpringBootApplication.exception;
 
+
+/**
+ * Classe di eccezione che viene lanciata nel caso in cui vengano restituiti risultati errati durante la richiesta delle stats.
+ * @author Alessio Giacconi
+ * @author Erxhes Dedja
+ *
+ */
 public class StatsErrorException extends Exception{
 
-	/**
-	 * 
-	 */
+	
 	private static final long serialVersionUID = 1L;
+	
+	/**
+	 * costruttore eccezione, che richiama costruttore della superclasse 
+	 * @param messaggio
+	 */
 
 	public StatsErrorException() {
 		super();
