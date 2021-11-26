fc9b0b6b7b54a4e8b15b63a5fc163c6b4b786da6
Erxhes Dedja <91878765+Erxhes@users.noreply.github.com>
2021-11-26 16:13:30 +0100

    Update InvalidBodyException.java

--- SpringBootApplication/src/main/java/it/univpm/SpringBootApplication/exception/InvalidBodyException.java
+++ SpringBootApplication/src/main/java/it/univpm/SpringBootApplication/exception/InvalidBodyException.java
@@ -3,14 +3,29 @@ package it.univpm.SpringBootApplication.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.server.ResponseStatusException;
 
+/**
+ * Classe di eccezione che viene lanciata per gestire le richieste sbagliate nel body o nel caso di un body non valido.
+ * @author Alessio Giacconi
+ * @author Erxhes Dedja
+ *
+ */
+
 public class InvalidBodyException extends Exception{
 	
 
 	private static final long serialVersionUID = 1L;
+	
+	/**
+	 * costruttore eccezione, che richiama costruttore della superclasse 
+	 * @param messaggio
+	 */
 
 	public InvalidBodyException(String msg) {
+		
 		super();
+		
 		throw new ResponseStatusException(HttpStatus.BAD_REQUEST, msg);
 	}
 		
 }
+
