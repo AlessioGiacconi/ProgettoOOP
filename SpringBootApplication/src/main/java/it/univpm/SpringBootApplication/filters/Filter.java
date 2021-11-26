17b33fd853dbfc67120ceda5be06145ff715ca49
Erxhes Dedja <91878765+Erxhes@users.noreply.github.com>
2021-11-26 16:24:02 +0100

    Update Filter.java

--- SpringBootApplication/src/main/java/it/univpm/SpringBootApplication/filters/Filter.java
+++ SpringBootApplication/src/main/java/it/univpm/SpringBootApplication/filters/Filter.java
@@ -18,6 +18,14 @@ import it.univpm.SpringBootApplication.service.Parse;
 import it.univpm.SpringBootApplication.service.url;
 import it.univpm.SpringBootApplication.statistics.Stats;
 
+
+/**
+ * Classe contenente metodi per il filtraggio delle offerte di lavoro.
+ * @author Alessio Giacconi
+ * @author Erxhes Dedja
+ *
+ */
+
 public class Filter {
 
 	ApiCall call = new ApiCall();
@@ -28,6 +36,14 @@ public class Filter {
 	Stats stats = new Stats();
 	Vector<String> cities = c.getNames();
 	
+	/**
+	 * Metodo che restituisce gli url con tutti i lavori filtrati in base alle richieste possibili.
+	 * @return ArrayList contenente gli url filtrati in base alle richieste possibili.
+	 * @throws InvalidBodyException body vuoto o null.
+	 * @throws JobsNotFoundException nel caso in cui non viene trovato un lavoro con i filtri richiesti.
+	 */
+	
+	
 	public ArrayList<Job> filteredJobs(Map<String,Object> body) throws ParseException, InvalidBodyException, JobsNotFoundException {
 		
 		call.readAPIKey();
@@ -35,68 +51,129 @@ public class Filter {
 		String url = new String();
 		String data = new String();
 		boolean flag = false;
+		
 		if (body.isEmpty()) {
+			
 			finalOutput = fjobs.getJobs();
-		}else {
+			
+		} else {
+			
 			if(((String)body.get("location")).equals("")) {
+				
 				flag = true;
 			}
+			
 			for(String l : cities) {
+				
 			if (((String)body.get("location")).equalsIgnoreCase(l)) {
+				
 				flag = true;
 			}
 		}
 		if ((flag == true) && (((String)body.get("employment_type")).equalsIgnoreCase("full time")) == true || (((String)body.get("employment_type")).equalsIgnoreCase("contract")) == true  || (((String)body.get("employment_type")).equals(""))){
+			
 			if((body.get("location") != "") && (body.get("remote") != null) && (body.get("employment_type") != "")) {
+				
 			url = u.LocRemPtFt((String)body.get("location"), (boolean)body.get("remote"), (String)body.get("employment_type"));
+				
 			data = call.callAPI(url);
+				
 			finalOutput = parse.Parsing(data);
-		}else if((body.get("location") != "") && (body.get("remote") != null) && (body.get("employment_type") == "")) {
-			url = u.LocRem((String)body.get("location"), (boolean)body.get("remote"));
-			data = call.callAPI(url);
-			finalOutput = parse.Parsing(data);
-		}else if((body.get("location") != "") && (body.get("remote") == null) && (body.get("employment_type") == "")) {
-			url = u.Loc((String)body.get("location"));
-			data = call.callAPI(url);
-			finalOutput = parse.Parsing(data);
-		}else if((body.get("location") != "") && (body.get("remote") == null) && (body.get("employment_type") != "")) {
-			url = u.LocPtFt((String)body.get("location"), (String)body.get("employment_type"));
-			data = call.callAPI(url);
-			finalOutput = parse.Parsing(data);
+				
+		}
+			else 
+			        if((body.get("location") != "") && (body.get("remote") != null) && (body.get("employment_type") == "")) {
+				
+			        url = u.LocRem((String)body.get("location"), (boolean)body.get("remote"));
+				
+			        data = call.callAPI(url);
+				
+			        finalOutput = parse.Parsing(data);
+				
+		}       else 
+		  		if((body.get("location") != "") && (body.get("remote") == null) && (body.get("employment_type") == "")) {
+					
+			        url = u.Loc((String)body.get("location"));
+					
+			        data = call.callAPI(url);
+					
+			       finalOutput = parse.Parsing(data);
+			
+					
+		}      else 
+			      if((body.get("location") != "") && (body.get("remote") == null) && (body.get("employment_type") != "")) {
+				      
+			      url = u.LocPtFt((String)body.get("location"), (String)body.get("employment_type"));
+				      
+			      data = call.callAPI(url);
+				      
+			      finalOutput = parse.Parsing(data);
+				 
 		}else if((body.get("location") == "") && (body.get("remote") != null) && (body.get("employment_type") == "")) {
+				
 			for(String s : cities) {
+				
 					url = u.LocRem(s, (boolean)body.get("remote"));
+				
 					data = call.callAPI(url);
+				
 					ArrayList<Job> output = parse.Parsing(data);
+				
 					finalOutput.addAll(output);
 			}
-		}else if((body.get("location") == "") && (body.get("remote") == null) && (body.get("employment_type") != "")) {
-			for(String s : cities) {
+				
+		} else 
+			if((body.get("location") == "") && (body.get("remote") == null) && (body.get("employment_type") != "")) { 
+				
+			 for(String s : cities) {
+				 
 				url = u.LocPtFt(s, (String)body.get("employment_type"));
+				 
 				data = call.callAPI(url);
+				 
 				ArrayList<Job> output = parse.Parsing(data);
+				 
 				finalOutput.addAll(output);
 			}	
-		}else if((body.get("location") == "") && (body.get("remote") != null) && (body.get("employment_type") != "")) {
+				
+		}else 
+			if((body.get("location") == "") && (body.get("remote") != null) && (body.get("employment_type") != "")) {
+				
 			for(String s : cities) {
+				
 				url = u.LocRemPtFt(s, (boolean)body.get("remote"), (String)body.get("employment_type"));
+				
 				data = call.callAPI(url);
+				
 				ArrayList<Job> output = parse.Parsing(data);
+				
 				finalOutput.addAll(output);
 			}
 		}
 	if(finalOutput.isEmpty()) {
+		
 		String msg = "Jobs not found..";
+		
 		throw new JobsNotFoundException(msg);
 	}
-		}else {
+		}
+			else {
+				
 			throw new InvalidBodyException("An invalid body has been inserted..");
 		}
+			
 		}
+		
 	return finalOutput;	
 		
 		
 	}
+	
+	/**
+	 * Metodo che restituisce le statistiche filtrate in base a location, num_employees e date.
+	 * @return ArrayList contenente gli url con le statistiche filtrate in base alle richieste possibili.
+	 */
+
 	
 	
 	public ArrayList<StatsParameters> filteredStats(Map<String,Object> body) throws ParseException, JobsNotFoundException, InvalidBodyException, java.text.ParseException, StatsErrorException{
@@ -106,57 +183,107 @@ public class Filter {
 		String data = new String();
 		ArrayList<StatsParameters> finalOutputStats = new ArrayList<StatsParameters>();
 		boolean flag = false; 
+		
 		if(body.isEmpty()) {
 			finalOutputStats = stats.getStats();
-		}else {
+			
+		} else {
+			
 			if(((String)body.get("location")).equals("")) {
+				
 				flag = true;
 			}
+			
 			for(String s : cities) {
+				
 				if(((String)body.get("location")).equalsIgnoreCase(s)) {
+					
 					flag = true;
 				}
 			}
+			
 			if(((String)body.get("date")).length() > 10) {
+				
 				String msg = "An invalid body has been inserted..";
+				
 				throw new InvalidBodyException(msg);
+				
 			}
+			
 		if((flag == true) && (((String)body.get("num_employees")).equals("1-10") == true || ((String)body.get("num_employees")).equals("11-50") == true || ((String)body.get("num_employees")).equals("51-100") == true || ((String)body.get("num_employees")).equals("101-250") == true || ((String)body.get("num_employees")).equals("") == true)){
 			if((body.get("location") != "") && (body.get("num_employees") != "") && (body.get("date") != "")){
+				
 			url = u.LocNumEmp((String)body.get("location"), (String)body.get("num_employees"));
+				
 			data = call.callAPI(url);
+				
 			finalOutputStats = parse.StatsParsing(data, (String)body.get("location"), (String)body.get("date"));
-		}else if((body.get("location") != "") && (body.get("num_employees") != "") && (body.get("date") == "")){
+				
+		} else 
+			if((body.get("location") != "") && (body.get("num_employees") != "") && (body.get("date") == "")) {
+				
 			url = u.LocNumEmp((String)body.get("location"),(String)body.get("num_employees"));
+				
 			data = call.callAPI(url);
+				
 			finalOutputStats = parse.StatsParsing(data, (String)body.get("location"));
-		}else if((body.get("location") != "") && (body.get("num_employees") == "") && (body.get("date") == "")) {
+				
+		} else 
+			if((body.get("location") != "") && (body.get("num_employees") == "") && (body.get("date") == "")) {
+				
 			url = u.Loc((String)body.get("location"));
+				
 			data = call.callAPI(url);
+				
 			finalOutputStats = parse.StatsParsing(data, (String)body.get("location"));
-		}else if((body.get("location") == "") && (body.get("num_employees") != "") && (body.get("date") == "")) {
+				
+		} else 
+			if((body.get("location") == "") && (body.get("num_employees") != "") && (body.get("date") == "")) {
+				
 			for(String s : cities) {
+				
 				url = u.LocNumEmp(s, (String)body.get("num_employees"));
+				
 				data = call.callAPI(url);
+				
 				ArrayList<StatsParameters> OutputStats = parse.StatsParsing(data, s);
+				
 				finalOutputStats.addAll(OutputStats);
+				
 			}
-		}else if((body.get("location") == "") && (body.get("num_employees") != "") && (body.get("date") != "")) {
+		 }else 
+			if((body.get("location") == "") && (body.get("num_employees") != "") && (body.get("date") != "")) {
+				
 			for(String s : cities) {
+				
 				url = u.LocNumEmp(s, (String)body.get("num_employees"));
+				
 				data = call.callAPI(url);
+				
 				ArrayList<StatsParameters> OutputStats = parse.StatsParsing(data, s, (String)body.get("date"));
+				
 				finalOutputStats.addAll(OutputStats);
 			}
-		}else if((body.get("location") != "") && (body.get("num_employees") == "") && (body.get("date") != "")) {
+		} else 
+			if((body.get("location") != "") && (body.get("num_employees") == "") && (body.get("date") != "")) {
+				
 			url = u.Loc((String)body.get("location"));
+				
 			data = call.callAPI(url);
+				
 			finalOutputStats = parse.StatsParsing(data, (String)body.get("location"), (String)body.get("date"));
-		}else if((body.get("location") == "") && (body.get("num_employees") == "") && (body.get("date") != "")) {
+				
+		} else
+			if((body.get("location") == "") && (body.get("num_employees") == "") && (body.get("date") != "")) {
+				
 			for(String s : cities) {
+				
 				url = u.Loc(s);
+				
 				data = call.callAPI(url);
+				
 				ArrayList<StatsParameters> OutputStats = parse.StatsParsing(data, s, (String)body.get("date"));
+				
 				finalOutputStats.addAll(OutputStats);
 			}
 		}
@@ -165,11 +292,15 @@ public class Filter {
 			String msg = "An error occured during elaboration of stats..";
 			throw new StatsErrorException(msg);
 		}
-		}else {
+			 
+		} else {
+			
 			String msg = "An invalid body has been inserted..";
+			
 			throw new InvalidBodyException(msg);
 		}
 		}
 		return finalOutputStats;
 	}
 }
+
