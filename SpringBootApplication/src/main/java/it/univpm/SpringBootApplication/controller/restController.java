1f3b41cb93fc7c634b06cc1b27be4609b8e7ade1
Erxhes Dedja <91878765+Erxhes@users.noreply.github.com>
2021-11-26 16:11:40 +0100

    Update restController.java

--- SpringBootApplication/src/main/java/it/univpm/SpringBootApplication/controller/restController.java
+++ SpringBootApplication/src/main/java/it/univpm/SpringBootApplication/controller/restController.java
@@ -21,6 +21,11 @@ import it.univpm.SpringBootApplication.service.FindJobs;
 import it.univpm.SpringBootApplication.service.Parse;
 import it.univpm.SpringBootApplication.statistics.Stats;
 
+/**
+ * Rappresenta la classe che gestisce tutte le chiamate
+ * @author Alessio Giacconi
+ * @author Erxhes Dedja
+ */
 
 @RestController
 public class restController {
@@ -28,6 +33,10 @@ public class restController {
 	FindJobs fjobs = new FindJobs();
 	Filter filter = new Filter();
 	Stats stats = new Stats();
+	
+	/**Rotta di tipo GET che ritorna tutte le offerte di lavoro presenti su FindWork.
+	 * @return JsonArray contenente le offerte di lavoro
+	 */	
 	
 	@GetMapping(value="/Jobs")
 	public ResponseEntity<Object> jobList() throws JobsNotFoundException {
@@ -40,10 +49,18 @@ public class restController {
 		}
 	}
 	
+	/**Rotta di tipo GET che ritorna tutte le città in cui è possibile cercare i vari lavori.
+	 */	
+	
 	@GetMapping(value = "/Cities")
 	public City getCities() {
 		return new City();
 	}
+	
+	/**Rotta di tipo POST che effettua il filtraggio delle offerte di lavoro in base a location, lavoro in remoto e non, part-time o full-time.
+	 * Le parole chiave vengono prelevate dal body della richiesta.
+	 * @return JSONArray con le offerte che soddisfano la richiesta.
+	 */
 	
 	@PostMapping(value="/Filters")
 	public ResponseEntity<Object> filteredJobList(@RequestBody JSONObject body) throws InvalidBodyException, JobsNotFoundException{
@@ -56,6 +73,14 @@ public class restController {
 		}
 	}
 	
+	
+	/**
+	 * Rotta di tipo GET che effettua il calcolo delle statistiche.
+	 * Ci permette di generare statistiche sui lavori trovati, riguardanti la tipologia (quantità e percentuale di lavori in remoto e non), 
+	 * gli altri linguaggi richiesti insieme al python
+	 * @return Ritornano le statistiche effettuate.
+	 */
+	
 	@GetMapping(value="/allStats")
 	public ResponseEntity<Object> statsList() throws JobsNotFoundException{
 		try {
@@ -66,6 +91,14 @@ public class restController {
 			return new ResponseEntity<>(e, HttpStatus.BAD_REQUEST);
 		}
 	}
+	
+	
+	/**Rotta di tipo POST che effettua il filtraggio delle statistiche in base 
+	 * ad una o più città, 
+	 * data di creazione degli annunci, 
+	 * grandezza azienda (in base al numero di impiegati).
+	 * @return le statistiche che soddisfano le richieste.
+	 */
 	
 	@PostMapping(value="/filterStats")
 	public ResponseEntity<Object> filteredStats(@RequestBody JSONObject body) throws JobsNotFoundException, InvalidBodyException, java.text.ParseException, StatsErrorException{
