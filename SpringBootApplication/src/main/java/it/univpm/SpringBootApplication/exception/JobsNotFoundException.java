282b8b1d3f69cbdde12f8e37045d87f1bd2d2780
Erxhes Dedja <91878765+Erxhes@users.noreply.github.com>
2021-11-26 16:15:43 +0100

    Update JobsNotFoundException.java

--- SpringBootApplication/src/main/java/it/univpm/SpringBootApplication/exception/JobsNotFoundException.java
+++ SpringBootApplication/src/main/java/it/univpm/SpringBootApplication/exception/JobsNotFoundException.java
@@ -1,8 +1,22 @@
 package it.univpm.SpringBootApplication.exception;
 
+
+/**
+ * Classe di eccezione che viene lanciata se le richieste non vengono soddisfatte e non viene trovato il lavoro.
+ * @author Alessio Giacconi
+ * @author Erxhes Dedja
+ *
+ */
+
+
 public class JobsNotFoundException extends Exception {
 	
 	private static final long serialVersionUID = 1L;
+	
+	/**
+	 * costruttore eccezione, che richiama costruttore della superclasse 
+	 * @param messaggio
+	 */
 	
 	public JobsNotFoundException() {
 		super();
